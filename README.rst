870bd8062916d32b371a7e841069411c028e49a2
Duncan Macleod <duncan.macleod@ligo.org>
2018-03-18 18:31:29 -0700

    setup.py: first cut at packaging for gwin

---
+++ README.rst
@@ -0,0 +1,8 @@
+A python package for Bayesian Inference of gravitational-wave experimental data.
+
+Installation
+=============
+
+::
+
+   pip install gwin
---
+++ setup.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018  Collin Capano
+#
+# This file is part of GWIn
+#
+# GWIn is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GWIn is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GWIn.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Setup the GWIn package
+"""
+
+import os.path
+import re
+import sys
+
+from setuptools import (setup, find_packages)
+
+
+# -- versioning ---------------------------------------------------------------
+
+# get version
+def find_version(path):
+    with open(path, 'r') as fp:
+        version_file = fp.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+__version__ = find_version(os.path.join('gwin', '__init__.py'))
+
+# -- dependencies -------------------------------------------------------------
+
+setup_requires = [
+    'setuptools',
+]
+
+install_requires = [
+    'pycbc',
+    'matplotlib',
+    'scipy',
+    'h5py',
+    'corner',
+]
+
+extras_require = {
+    'kombine': ['kombine'],
+    'emcee': ['emcee'],
+}
+
+
+# -- files --------------------------------------------------------------------
+
+def find_scripts(scripts_dir='bin'):
+    """Get relative file paths for all files under the ``scripts_dir``
+    """
+    scripts = []
+    for (dirname, _, filenames) in os.walk(scripts_dir):
+        scripts.extend([os.path.join(dirname, fn) for fn in filenames])
+    return scripts
+
+
+# -- setup --------------------------------------------------------------------
+
+# get long description from README
+with open('README.rst', 'rb') as f:
+    longdesc = f.read().decode().strip()
+
+# run setup
+setup(
+    name='gwin',
+    version=__version__,
+    description="A python package for Bayesian inference of "
+                "gravitational-wave data",
+    long_description=longdesc,
+    author='Collin Capano',
+    author_email='collin.capano@ligo.org',
+    url='https://github.com/gwin/gwin',
+    license='GPLv3',
+    packages=find_packages(),
+    scripts=find_scripts(),
+    setup_requires=setup_requires,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
